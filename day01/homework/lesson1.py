937a03ebf42f8c5c759583afa92b980ca91e3b34
DeaDevidze <devidzedea9@gmail.com>
2024-10-09 20:44:01 +0400

    second commit

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,34 @@
+name = "dea" 
+#name  არის ცვლადი 
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "dea" არის ცვლადისთვის მნიშვნელობა
+
+surname = "devidze"
+print (name)
+# პრინტ უნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+age = 15  # ეს არის int ( interger ) მთელი რიცხვი
+height = 168.5 # ეს არის float ტიპის ცვლადი ( ათწილადი)
+# booLean (booL) ტიპის ცვლადი 
+
+knows_programming = True # True ან False
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+is_Ugly = False # ჯავასკრიპტული comeLcase
+
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print (name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surmane))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + surname + " " + "is" +" "+ str(age) + " " + "years old " + "and" + " " +  str(height)
+ + " " + "cm")
+
+
