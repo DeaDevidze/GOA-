2e474f989cd5d419f8103ba643f651c58158fcfe
DeaDevidze <devidzedea9@gmail.com>
2024-10-10 23:50:46 +0400

    second commit

---
+++ day02/homework/lesson2.py
@@ -0,0 +1,29 @@
+name = "lizi"
+surname = "devidze"
+age = 21
+print(name + " " + surname + " " + "is" + str(age) + "years old")
+
+
+name = "mariam"
+surname = "kalandia"
+age = 15
+print(name + " " + surname + " " + "is" + str(age) + "years old")
+
+
+name = "nini"
+surname = "datuashvili"
+age = 15
+print(name + " " + surname + " " + "is" + str(age) + "years old")
+
+
+name = "mirian"
+surname = "devidze"
+age = 45
+print(name + " " + surname + " " + "is" + str(age) + "years old")
+
+
+name = "anano"
+surname = "morokhia"
+age = 15
+print(name + " " + surname + " " + "is" + str(age) + "years old")
+
