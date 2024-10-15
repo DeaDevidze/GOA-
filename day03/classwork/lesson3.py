3cfaccc5fdfeba5e530c24154e3bc729c08f6cf4
DeaDevidze <devidzedea9@gmail.com>
2024-10-16 00:24:33 +0400

    second commit

---
+++ day03/classwork/lesson3.py
@@ -0,0 +1,32 @@
+num1 = 2
+num2 = 4
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+lastname = "devidze"
+age = 15
+
+firstname =  "mari"
+
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+
+#დეკლარაცია - ცვლადის შექმნა (ტოლობის მარცხენა მახრე)
+#ინილიზაცია - მნიშვნელობის მინიჭება (ტოლობის მარჯვენა მხარე)
+#რე-ინციციალიზება ცვლადებისთვის მნიშვნელობის შეცვლას (თავიდან მინიჭება)
+
+
+
+
---
+++ day03/homework/lesson3.py
@@ -0,0 +1,59 @@
+num1 = 5
+num2 = 10
+num3 = 15
+
+print(num1 + num2 + num3)
+
+#1) ცვლადის სახელის მოფიქრება
+#2) უდრის, მინიჭების ოპერატორის დაწერე
+#3) მნიშვნელობის მინიჭება
+
+x = 15
+y = 3
+print(x*y)
+
+lenght = 10
+width = 20
+print(20+10+20+10)
+
+age1 = 15
+age2 = 21
+age3 = 45
+print(age1 + age2 + age3 / 3)
+
+name = "dea"
+surname = "devidze"
+print("dea" + "devidze")
+
+
+age = 15
+fullname = "dea"
+print(fullname * age)
+
+#დეკლარაცია-ცვლადის შექმნა
+#ინიციალიზება-ცვლადისთვის მნიშვნელობის შექმნა
+#რეინიციალიზება-შექმნილი ცვლადის მნიშვნელოის შეცვლა
+
+firstname = 15
+firstname = "dea"
+
+age = 20
+print(age)
+
+age =10
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
