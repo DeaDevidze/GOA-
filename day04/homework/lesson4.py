c8c5e0214a126f2b7c0d7de0620519446d092813
DeaDevidze <devidzedea9@gmail.com>
2024-10-16 20:26:12 +0400

    second commit

---
+++ day04/homework/lesson4.py
@@ -0,0 +1,30 @@
+#bug-როდესაც შეცდომა აღმოჩენილია კომპიუტერისთვის მიცემულ ინსტრუქციების ერთობლიობაში
+#debugging-კომპიუტერის ინსტრუქციების ერთობლიობაში შეცდომის პოვნის პროცესი
+
+#print(dea)
+print("dea")
+#score 100
+score = 100
+#city=paris
+city = "paris"
+
+x = 4
+y = 5
+print(x*y)
+num1 = 2
+num2 = 5
+print(num1/num2)
+num1 = 23
+num2 = 11
+print(num1-num2)
+x = 15
+y = 15
+print(x-y)
+
+miriani = 45
+lizi =21
+erekle = 21
+tamuna = 46
+nini = 17
+ani = 14
+print( miriani + lizi + tamuna + erekle + nini + ani )
