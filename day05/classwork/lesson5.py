55b6601438408c52d85c35244fded8fb137b8055
DeaDevidze <devidzedea9@gmail.com>
2024-10-18 01:06:24 +0400

    second commit

---
+++ day05/classwork/lesson5.py
@@ -0,0 +1,53 @@
+# 1) შეასწორეთ (debug) შემდეგი კოდი;
+
+name = "alice"
+print(name)
+
+salary = 75000
+age = 30
+role = "software engineer"
+print("salary")
+print(role)
+
+# შეასწორეთ კოდი:
+
+balance = 750
+cost = 400
+print(balance - cost)
+
+
+# python
+
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+
+
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+#როდესაც ვბეჭდავთ ყოველთვის გვჭიდება ბრჩხილები print-თან
+
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+#სწორია
+
+
+temperature = 30
+print("temperature")
+#აქ შეცდონა ის იყო რომ ცვლადი ბრჩხილებში არ იყო ჩასმული, რადგან ეს არის სტრინგი
+
+#enter your age
+age = input("what is your age?")
+print("your age is " + " " + (age))
+
+colour = input("whta is your favorite colour?")
+print("your favorite colour is" + " " + colour)
+
+car = input("what is your favorite car brand?")
+print("nice choosen" + " ")
---
+++ day05/homework/lesson5.py
@@ -0,0 +1,13 @@
+
+name = input("Enter your first name:lizi")              #მომხმარებელს მოეთხოვება შეიყვანოს თავისი სახელი და ეს მნიშვნელობა შეინახება ცვლადში name
+surname = input("Enter your last name:devidze")             # surname
+age = int(input("Enter your age: 21"))                    # age
+residence = input("Enter your residence: tbilisi")        # residance
+email = input("Enter your email:devidzelizi@gamil.com ")      # email
+
+
+# მომხმარებელი შეიყვანს ასაკს და int() გამოიყენება რომ ასაკი გადაყვანილ იქნას მთელ რიცხვად
+
+age = int(input("enter your age: "))   
+age_in_20_years = age + 20                   
+print(f"In 20 years, you will be {age_in_20_years} years old.") 
